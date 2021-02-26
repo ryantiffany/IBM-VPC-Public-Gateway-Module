d0dabcb8deee3ccfcfd94fde3594b6c06fb4de64
Ryan Tiffany <79592153+ryantiffany@users.noreply.github.com>
2021-02-26 16:08:51 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output id {
   value = ibm_is_public_gateway.gateway.id
 }
 
-output gateway_floating_ip {
-  value = ibm_is_public_gateway.gateway.floating_ip
+output gateway_ip_address {
+  value = ibm_is_public_gateway.gateway.floating_ip.address
+}
+
+output gateway_ip_id {
+  value = ibm_is_public_gateway.gateway.floating_ip.id
 }
