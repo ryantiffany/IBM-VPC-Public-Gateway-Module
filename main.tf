cd0fa2d1cdbda94d4e4aa7b1a2e437ff48cb4614
Ryan Tiffany <79592153+ryantiffany@users.noreply.github.com>
2021-03-05 09:04:06 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ resource ibm_is_public_gateway gateway {
   zone           = var.zone
   resource_group = var.resource_group_id
   tags           = var.tags
-  
-  floating_ip {
-    address = local.floating_ip_address
-    }
 }
 
-locals {
-  floating_ip_address     = var.floating_ip_address != "" ? var.floating_ip_address : null
-}
+
